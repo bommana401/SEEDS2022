5d92b83edce5731269cab0aa8db06eaafc3b1adf
jolieip <ipjolie1507@gmail.com>
2022-08-02 19:55:34 -0700

    commit project

---
+++ app/src/main/java/com/example/tiptome/Post.kt
@@ -0,0 +1,10 @@
+package com.example.tiptome
+
+data class Post(
+    var uid: String? = "",
+    var author: String? = "",
+    var title: String? = "",
+    var body: String? = "",
+    var starCount: Int = 0,
+    var stars: MutableMap<String, Boolean> = HashMap()
+)
